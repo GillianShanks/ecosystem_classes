5c59bcc6114510cfff3b8fd3e6e3b6437e8ee966
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-23 19:32:58 +0100

    Updated River to have a populated fish array, passed fish count test

--- River.rb
+++ River.rb
@@ -1,16 +1,16 @@
 class River
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, fish)
     @name = name
-    @fish = []
+    @fish = fish #array
   end
 
   def fish_count()
     return @fish.count()
   end
 
-  def add_fish(fish)
-    @fish << fish
-  end
+  # def add_fish(fish)
+  #   @fish << fish
+  # end
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -6,25 +6,26 @@ require_relative("../Fish.rb")
 class RiverTest < MiniTest::Test
 
   def setup()
-    @river1 = River.new("Amazon")
-
     @fish1 = Fish.new("Nemo")
     @fish2 = Fish.new("Dory")
     @fish3 = Fish.new("Marlin")
+
+    fish = [@fish1, @fish2, @fish3]
+    @river1 = River.new("Amazon", fish)
   end
 
   def test_can_get_name()
     assert_equal("Amazon", @river1.name)
   end
 
-  def test_river_is_empty()
-    assert_equal(0, @river1.fish_count())
+  def test_river_has_fish()
+    assert_equal(3, @river1.fish_count())
   end
-
-  def test_river_add_fish()
-    @river1.add_fish(@fish1)
-    @river1.add_fish(@fish2)
-    assert_equal(2, @river1.fish_count())
-  end
+  #
+  # def test_river_add_fish()
+  #   @river1.add_fish(@fish1)
+  #   @river1.add_fish(@fish2)
+  #   assert_equal(5, @river1.fish_count())
+  # end
 
 end
